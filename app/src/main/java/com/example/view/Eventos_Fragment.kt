9cdefb5266e3b08b4c743a72ce43fa9027e208d7
angelDguez9 <79721414+angelDguez9@users.noreply.github.com>
2021-03-07 00:13:33 -0600

    "update2"

---
+++ app/src/main/java/com/example/view/Eventos_Fragment.kt
@@ -0,0 +1,19 @@
+package com.example.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Eventos_Fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eventos_, container, false)
+    }
+}
